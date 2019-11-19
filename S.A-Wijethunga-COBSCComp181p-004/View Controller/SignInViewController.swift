1f7e4043c17bd609e8fae39c0d4d771e3c69de79
Manjula Devapura <manjuladevapura@gmail.com>
2019-11-19 19:26:06 +0530

    new

---
+++ S.A-Wijethunga-COBSCComp181p-004/View Controller/SignInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignInViewController.swift
+//  S.A-Wijethunga-COBSCComp181p-004
+//
+//  Created by Digital-02 on 11/19/19.
+//  Copyright © 2019 Digital-02. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
